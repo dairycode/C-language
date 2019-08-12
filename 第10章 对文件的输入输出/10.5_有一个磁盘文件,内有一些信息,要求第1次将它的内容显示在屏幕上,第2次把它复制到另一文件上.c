bc1565bc73a1e9056c04d151b80d4b036b2119fb
刘泽 <liuze@promote.cache-dns.local>
2019-08-12 16:15:53 +0800

    C语言学习代码

---
+++ 第10章 对文件的输入输出/10.5_有一个磁盘文件,内有一些信息,要求第1次将它的内容显示在屏幕上,第2次把它复制到另一文件上.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	FILE *fp1,*fp2;
+	fp1=fopen("file1.dat","r");
+	fp2=fopen("file2.dat","w");
+	char ch=getc(fp1);
+	while(!feof(fp1))
+	{
+		putchar(ch);
+		ch=getc(fp1);
+	}
+	putchar(10);
+	rewind(fp1);
+	ch=getc(fp1);
+	while(!feof(fp1))
+	{
+		fputc(ch,fp2);
+		ch=fgetc(fp1);
+	}
+	fclose(fp1);fclose(fp2);
+	return 0;
+}
